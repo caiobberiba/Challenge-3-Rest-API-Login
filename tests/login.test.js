afe85703582ec6694dd4a35e731b966ad3855509
CarlosBerenguer <carloshfb03@gmail.com>
2025-07-22 15:15:31 -0300

    removing login failed scenario so other members can participate more

--- tests/login.test.js
+++ tests/login.test.js
@@ -17,43 +17,5 @@ describe("Login", () => {
             expect(response.status).to.be.equal(200)
             expect(response.body.message).to.be.equal("Login successful.")
         })
-
-        it("Must return 400 and user not found", async () => {
-            const response = await request(process.env.BASE_URL)
-                .post('/auth/login')
-                .set('Content-type', 'application/json')
-                .send({
-                    username: 'shanks123',
-                    password: '123456'
-                });
-                expect(response.status).to.be.equal(400)
-                expect(response.body).to.have.property("error", "Invalid username or password.")
-        })
-
-        it("Must return 400 and password is incorrect", async () => {
-            const response = await request(process.env.BASE_URL)
-                .post('/auth/login')
-                .set('Content-type', 'application/json')
-                .send({
-                    username: 'shanks',
-                    password: '1234567'
-                });
-                expect(response.status).to.be.equal(400)
-                expect(response.body.error).to.be.equal( "Invalid username or password.")
-
-        })
-
-        it("Must return 400 and username is incorrect", async () => {
-            const response = await request(process.env.BASE_URL)
-                .post('/auth/login')
-                .set('Content-type', 'application/json')
-                .send({
-                    username: 'shanks123',
-                    password: '123456'
-                });
-                expect(response.status).to.be.equal(400)
-                expect(response.body.error).to.be.equal( "Invalid username or password.")
-
-        });
     })
 })
