d5536e90ea1d3138cd6f6855eabb68208e5474ee
CarlosBerenguer <carloshfb03@gmail.com>
2025-07-22 19:56:18 -0300

    adding login failed test - invalid user

--- tests/login.test.js
+++ tests/login.test.js
@@ -17,5 +17,18 @@ describe("Login", () => {
             expect(response.status).to.be.equal(200)
             expect(response.body.message).to.be.equal("Login successful.")
         })
+
+        it("Must return 400 and user not found", async () => {
+            const response = await request(process.env.BASE_URL)
+                .post('/auth/login')
+                .set('Content-type', 'application/json')
+                .send({
+                    username: 'marcio',
+                    password: '123456'
+                });
+
+            expect(response.status).to.be.equal(400)
+            expect(response.body).to.have.property("error", 'Invalid username or password.')
+        })
     })
 })
