27186c84e274f0b13efe318259c412a7f35f91b4
CarlosBerenguer <carloshfb03@gmail.com>
2025-07-22 13:09:25 -0300

    WIP: Add remove endpoint tests / Skip tests which add data

--- tests/user.test.js
+++ tests/user.test.js
@@ -34,57 +34,29 @@ describe("User", () => {
             //expect (response.body).to.have.property("error", "User already exists."); //it works too, just another way to do it
         })
     })
-
-    describe("POST /auth/login", () => {
-        it("Must return 200 and user logged in successfully", async () => {
+    
+    describe("DELETE /auth/delete/{username}", () => {
+        it.skip("Must return 200 and user deleted successfully", async () => {
             const response = await request(process.env.BASE_URL)
-                .post('/auth/login')
+                .delete('/auth/delete/shanks')
                 .set('Content-type', 'application/json')
                 .send({
                     username: 'shanks',
-                    password: '123456'
                 });
 
-            expect(response.status).to.be.equal(200)
-            expect(response.body.message).to.be.equal("Login successful.")
+            expect(response.status).to.be.equal(200);
+            expect(response.body.message).to.be.equal("User deleted successfully.");
         })
-
         it("Must return 400 and user not found", async () => {
             const response = await request(process.env.BASE_URL)
-                .post('/auth/login')
+                .delete('/auth/delete/shanks123')
                 .set('Content-type', 'application/json')
                 .send({
                     username: 'shanks123',
-                    password: '123456'
                 });
-                expect(response.status).to.be.equal(400)
-                expect(response.body).to.have.property("error", "Invalid username or password.")
+
+            expect(response.status).to.be.equal(400);
+            expect(response.body).to.have.property("error", "User not found.");
         })
-
-        it("Must return 400 and password is incorrect", async () => {
-            const response = await request(process.env.BASE_URL)
-                .post('/auth/login')
-                .set('Content-type', 'application/json')
-                .send({
-                    username: 'shanks',
-                    password: '1234567'
-                });
-                expect(response.status).to.be.equal(400)
-                expect(response.body.error).to.be.equal( "Invalid username or password.")
-
-        })
-
-        it("Must return 400 and username is incorrect", async () => {
-            const response = await request(process.env.BASE_URL)
-                .post('/auth/login')
-                .set('Content-type', 'application/json')
-                .send({
-                    username: 'shanks123',
-                    password: '123456'
-                });
-                expect(response.status).to.be.equal(400)
-                expect(response.body.error).to.be.equal( "Invalid username or password.")
-
-        });
     })
-})  
+})
