2021340ef65a04064567d052c97ac14d68fd6a27
Márcio Corrêa <marcio.ramos.correa@gmail.com>
2025-08-07 14:33:40 -0300

    register and recover test

--- cypress/e2e/user/user.cy.js
+++ cypress/e2e/user/user.cy.js
@@ -1,11 +1,43 @@
 describe("User", () => {
-    it('should create a new user', () => {
-        cy.visit("http://localhost:3001");
-        cy.get("#createAccount").click();
-        cy.get("label[for='username']").type("shanks");
-        cy.get("label[for'email']").type("shanks@teste.com")
-        cy.get("label[for='password']").type("123456");
-        cy.get("label[for='confirmPassword']").type("123456");
-        cy.get("#createAccount").click();
+    describe("Register", () => {
+        it('should create a new user', () => {
+            cy.visit("http://localhost:3001");
+            cy.get("#createAccount").click();
+            cy.get("label[for='username']").type("shanks");
+            cy.get("label[for'email']").type("shanks@teste.com")
+            cy.get("label[for='password']").type("123456");
+            cy.get("label[for='confirmPassword']").type("123456");
+            cy.get("#createAccount").click();
+            cy.get("#successMessage").should("contain", "Account created successfully! You can now sign in.");
+        });
+        it('should give the error message "User already exists."', () => {
+            cy.visit("http://localhost:3001");
+            cy.get("#createAccount").click();
+            cy.get("label[for='username']").type("shanks");
+            cy.get("label[for'email']").type("shanks@teste.com")
+            cy.get("label[for='password']").type("123456");
+            cy.get("label[for='confirmPassword']").type("123456");
+            cy.get("#createAccount").click();
+            cy.get("#errorMessage").should("contain", "User already exists.");
+        });
+    });
+
+    describe("Recover Password", () => {
+        it('should recover the password', () => {
+            cy.visit("http://localhost:3001");
+            cy.get("#forgotPassword").click();
+            cy.get("label[for='username']").type("shanks");
+            cy.get("label[for'email']").type("shanks@teste.com")
+            cy.get("#recoverPassword").click();
+            cy.get("#successMessage").should("contain", "Password recovered successfully!");
+        });
+        it('should give the error message "User already exists."', () => {
+            cy.visit("http://localhost:3001");
+            cy.get("#forgotPassword").click();
+            cy.get("label[for='username']").type("shanks123");
+            cy.get("label[for'email']").type("shanks123@teste.com")
+            cy.get("#recoverPassword").click();
+            cy.get("#errorMessage").should("contain", "Password recovery failed. Please check your credentials.");
+        });
     });
 });
