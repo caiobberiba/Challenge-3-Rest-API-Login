974042255403e88a3575cd178439a134db924766
CarlosBerenguer <carloshfb03@gmail.com>
2025-08-08 18:41:23 -0300

    adicionando tela com lista de usuarios apos login no sistema e testes de login e da lista

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -1,8 +1,68 @@
 describe("Login", () => {
-    it("should login with valid credentials", () => {
+    beforeEach(() => {
         cy.visit("http://localhost:3001");
-        cy.get("label[for='username']").type("shanks");
-        cy.get("label[for='password']").type("123456");
+    });
+
+    it("should login with valid credentials and redirect to registered users page", () => {
+        cy.fixture('credentials').then(credentials => {
+            cy.get("label[for='username']").type(credentials.valid.username);
+            cy.get("label[for='password']").type(credentials.valid.password);
+        })
+
         cy.get("button[type='submit']").click();
+        cy.wait(1000);
+        cy.url().should("contain", "/users");
+        cy.get('.header > h4').should("contain.text", "Registered Users");
+    });
+
+    it("should login with invalid username", () => {
+        cy.fixture('credentials').then(credentials => {
+            cy.get("label[for='username']").type(credentials.invalid_username.username);
+            cy.get("label[for='password']").type(credentials.valid.password);
+        })
+        cy.get("button[type='submit']").click();
+        cy.wait(1000);
+        cy.url().should("contain", "http://localhost:3001");
+        cy.get("#errorMessage").should("contain.text", "Invalid username or password.");
+    });
+
+    it("should login with invalid password", () => {
+        cy.fixture('credentials').then(credentials => {
+            cy.get("label[for='username']").type(credentials.valid.username);
+            cy.get("label[for='password']").type(credentials.invalid_password.password);
+        })
+
+        cy.get("button[type='submit']").click();
+        cy.wait(1000);
+        cy.url().should("contain", "http://localhost:3001");
+        cy.get("#errorMessage").should("contain.text", "Invalid username or password.");
+    });
+    
+    // it("should login 3 times with invalid password", () => {
+    //     for (i=0; i < 3; i++)
+    //     {
+    //         cy.fixture('credentials').then(credentials => {
+    //             cy.get("label[for='username']").type(credentials.validButBlocked.username);
+    //             cy.get("label[for='password']").type(credentials.invalid_password.password);
+    //         })
+
+    //         cy.get("button[type='submit']").click();
+    //         cy.wait(1000);
+    //         cy.url().should("contain", "http://localhost:3001");
+            
+    //     }
+        
+        // cy.get("#errorMessage").should("contain.text", "'Account blocked due to too many attempts.'");
+    // });
+
+    it("should login with invalid username and password", () => {
+        cy.fixture('credentials').then(credentials => {
+            cy.get("label[for='username']").type(credentials.invalid_username.username);
+            cy.get("label[for='password']").type(credentials.invalid_password.password);
+        })
+        cy.get("button[type='submit']").click();
+        cy.wait(1000);
+        cy.url().should("contain", "http://localhost:3001");
+        cy.get("#errorMessage").should("contain.text", "Invalid username or password.");
     });
 });
--- cypress/e2e/user/user.cy.js
+++ cypress/e2e/user/user.cy.js
@@ -9,6 +9,10 @@ describe("User", () => {
             cy.registerUser();
             cy.get("#successMessage").should("contain", "Account created successfully! You can now sign in.");
         });
+        it('should create a new user to be blocked', () => {
+            cy.registerUserToBeBlocked();
+            cy.get("#successMessage").should("contain", "Account created successfully! You can now sign in.");
+        });
         it('should give the error message "User already exists."', () => {
             cy.registerUser();
             cy.get("#errorMessage").should("contain", "User already exists.");
@@ -27,6 +31,24 @@ describe("User", () => {
         it('should give the error message "Invalid data." when email is invalid', () => {
             cy.recoverPasswordWithInvalidEmail();
             cy.get("#errorMessage").should("contain", "Invalid data.");
+        });
+    });
+
+
+    describe("User list", () => {
+        it("should display the user list", () => {
+            cy.validLogin();
+            cy.get(".header > h4").should("contain.text", "Registered Users");
+            cy.get("#usersContainer").should("exist");
+            cy.get('.preloader-wrapper', { timeout: 5000 }).should('not.be.visible');
+
+            cy.fixture('credentials').then(credentials => {
+                const userList = cy.get("#usersContainer > .user-card");
+                userList.each((user) => {
+                    if (cy.wrap(user).find("span")?.value == credentials.valid.username)
+                        cy.wrap(user).should("have.text", credentials.valid.username);
+                });
+            })
         });
     });
 });
